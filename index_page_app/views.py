be4c47e3c00561df37b4db091095bc7466a8ce1f
isadev97 <isa325697@gmail.com>
2023-06-11 19:06:24 +0530

    template engine

--- index_page_app/views.py
+++ index_page_app/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index_page_view(request):
     data = {
+        "show_data" : False,
         "name" : "Ishjot",
-        "course" : "Python Backend Development live"
+        "course" : "Python Backend Development live",
+        "list" : [1, 2, 3, 4]
     }
     return render(request, "index.html", context=data)
     

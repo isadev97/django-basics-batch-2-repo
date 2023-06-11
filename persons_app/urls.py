60b6873d9142b6e7b2b4140fed96be6342b7c626
isadev97 <isa325697@gmail.com>
2023-06-11 19:32:57 +0530

    added a json response view

--- persons_app/urls.py
+++ persons_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from persons_app.views import index_view
+from persons_app.views import index_view, person_detail_view
 
 urlpatterns = [
     path("index/", index_view),
+    path("detail/<int:person_id>/", person_detail_view)
 ]
--- persons_app/views.py
+++ persons_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from persons_app.models import Person
+from django.http import HttpResponse, JsonResponse
+
 
 # Create your views here.
 def index_view(request):
@@ -9,3 +11,15 @@ def index_view(request):
     <h1>world</h1>
     """
     return HttpResponse(response)
+
+def person_detail_view(request, person_id):
+    try:
+        person_object = Person.objects.get(pk=person_id)
+    except Person.DoesNotExist:
+        return JsonResponse({"error" : True, "message" : "Person Does not exist", "status" : 404})
+    return JsonResponse({
+        "id": person_object.id,
+        "name": person_object.name,
+        "age": person_object.age,
+        "status": 200
+    })

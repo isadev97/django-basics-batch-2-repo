a13530163d5b4a1df4cce99f8b5610ee25977340
isadev97 <isa325697@gmail.com>
2023-06-11 20:22:52 +0530

    todo list view completed

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from todo_app.models import Todo
 
 # Create your views here.
 def list_view(request):
-    pass 
+    data = {
+        "todo_list" : Todo.objects.all().order_by("-created_at")
+    }
+    return render(request, "list.html", context=data)
 
 def create_view(request):
-    pass
+    todo_title = request.POST['todoTitle']
+    todo_desc = request.POST['todoDescription']
+    Todo.objects.create(
+        title=todo_title,
+        description=todo_desc,
+        completed=False
+    )
+    return redirect("todo_list_view")
 
 def update_view(request, todo_id):
     pass 

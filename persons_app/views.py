15715055812cf01496b46eb9f1c08188ea063083
isadev97 <isa325697@gmail.com>
2023-06-11 18:24:06 +0530

    updated .env

--- persons_app/views.py
+++ persons_app/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index_view(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    response = """ 
+    <h1>response</h1>
+    <h1>hello</h1>
+    <h1>world</h1>
+    """
+    return HttpResponse(response)
